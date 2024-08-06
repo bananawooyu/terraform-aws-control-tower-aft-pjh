082e982f7395d97c0a97cb666c967bbc5eace4e2
bananawooyu <57030333+bananawooyu@users.noreply.github.com>
2024-08-06 11:02:30 +0900

    change main.tf aft module configure

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ module "aft" {
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
   vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
+  account_request_repo_name                     = "${var.github_username}/terraform-aft-account-request-pjh"
+  account_provisioning_customizations_repo_name = "${var.github_username}/aft-account-provisioning-customizations-pjh"
   global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
   account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
 }
